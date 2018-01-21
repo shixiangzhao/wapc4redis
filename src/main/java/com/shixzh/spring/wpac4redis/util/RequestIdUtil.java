d8afaae4fee0ed299e8b1522860badbde77c7aa8
Shixiang Zhao <zsxfordream@163.com>
2018-01-21 18:51:42 +0800

    modified RequestIdUtil

--- src/main/java/com/shixzh/spring/wpac4redis/util/RequestIdUtil.java
+++ src/main/java/com/shixzh/spring/wpac4redis/util/RequestIdUtil.java
@@ -1,12 +1,31 @@
 package com.shixzh.spring.wpac4redis.util;
 
+import java.util.UUID;
+
 import javax.servlet.http.HttpServletRequest;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class RequestIdUtil {
 
-    public static String getRequestId(HttpServletRequest request) {
-        String requestId = null;
-        return null;
-    }
+	private static final Logger logger = LoggerFactory.getLogger(RequestIdUtil.class);
+	public static final String REQUEST_ID_KEY = "requestId";
+	public static ThreadLocal<String> requestIdThreadLocal = new ThreadLocal<String>();
+
+	public static String getRequestId(HttpServletRequest request) {
+		String requestId = null;
+		String parameterRequestId = request.getParameter(REQUEST_ID_KEY);
+		String headerRequestId = request.getHeader(REQUEST_ID_KEY);
+
+		if (parameterRequestId == null || headerRequestId == null) {
+			logger.info("request parameter 和 header 都没有requestId入参");
+			requestId = UUID.randomUUID().toString();
+		} else {
+			requestId = parameterRequestId != null ? parameterRequestId : headerRequestId;
+		}
+		requestIdThreadLocal.set(requestId);
+		return requestId;
+	}
 
 }
